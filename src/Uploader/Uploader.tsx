83eab06efe43748bea1ae161615e195f55a1462e
mj <lovely83@gmail.com>
2023-07-23 02:03:21 -0700

    added session id for clerk through headers

--- src/Uploader/Uploader.tsx
+++ src/Uploader/Uploader.tsx
@@ -69,6 +69,7 @@ export default class Uploader extends React.Component<UploaderProps, States> {
                             method: 'POST',
                             headers: {
                                 'Authorization': this.props.authToken,
+                                'SessionId': this.props.sessionId,
                                 'Access-Control-Allow-Origin': '*',
                             },
                             withCredentials: false,
@@ -95,6 +96,7 @@ export default class Uploader extends React.Component<UploaderProps, States> {
                             method: 'GET',
                             headers: {
                                 'Authorization': this.props.authToken,
+                                'SessionId': this.props.sessionId,
                                 'Access-Control-Allow-Origin': '*',
                             },
                             ondata: (formData) => {
